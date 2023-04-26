4dd940dc3b0427adcceaf780a932b5e8124de7ce
Dozer05 <131200020+Dozer05@users.noreply.github.com>
2023-04-26 16:52:27 -0700

    Add files via upload

---
+++ playerHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class playerHealth : MonoBehaviour
+{ 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TakeDamage(20);
+        }
+
+        void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+        }
+
+    }
+}
